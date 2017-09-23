562e47cc74497816961217d3051156657f992d9e
adamtre9115 <treadam9115@gmail.com>
2017-09-22 23:17:11 -0400

    update styling and logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -50,8 +50,8 @@ var questions = [{
         correctAnswer: "d"
     },
 ]
-var time = 5; //time allowed for each question
-var correct; // amount of correct responses
+var time = 30; //time allowed for each question
+var correct = 0; // amount of correct responses
 var incorrect; // amount of incorrect responses
 var countDown;
 var questionNum = 0;
@@ -59,67 +59,82 @@ var answers = [];
 
 $(document).ready(function () {
 
-    $("#gamebtn").on("click", function(){
+    $("#gamebtn").on("click", function () {
         $("#gamebtn").remove();
         $("#game").removeClass("text-center");
-        $("#game").addClass("gameBg");
         countDown = setInterval(qTimer, 1000);
-      
-        
-        // renderQuestion();
-        // qChange();
+        qdisplay();
     })
+
     function qTimer() {
         time--;
-        $("#timeDisplay").html("<p>There are " + time + " seconds remaining</p>");
-        renderQuestion();
-        if (time === 0) {
-            console.log("entered");
-            clearInterval(countDown);
-            
-        }
-    }
-    
-    // function qChange() {
-    //     var questionOutput = [];
-    //     questions.forEach(function (currentQuestion, questionNumber) {
-    //         var answers = [];
-    //         for (letter in currentQuestion.answers) {
-    //             answers.push(
-    //                 `<label class="form-check-label">
-    //                 <input type="radio" class="form-check-input" name="question${questionNumber}" value="${letter}">
-    //                 ${letter} :
-    //                 ${currentQuestion.answers[letter]}
-    //               </label>`
-    //             );
-    //         }
-    //         // add this question and its answers to the output
-    //         questionOutput.push(
-    //             `<div class="question"> ${currentQuestion.question} </div>
-    //             <div class="answers"> ${answers.join('')} </div>`
-    //         );
-    //         //   console.log(questionOutput);
-    //     })
-    //     $("#quiz").html(questionOutput);
-    // }
-    // qChange();
-    function renderQuestion (){
-        
-        $("#quiz").html(questions[questionNum].question);
+        // $("#timeDisplay").html("<p>There are " + time + " seconds remaining</p>");
 
-        if (time === 0){
-            questionNum++;
-            $("#quiz").empty();
-            $("#quiz").html(questions[questionNum].question);
-            time = 5;
-            qTimer();
-            
-        } else if (questionNum === questions.length && timer === 0){
+        if (time !== 0) {
+            $("#timeDisplay").html("<p>There are " + time + " seconds remaining</p>");
+            console.log("going");
+        } else {
+            $("#timeDisplay").html("<p>Time's Up!</p>");
+            console.log("stopped");
+            showResults();
             clearInterval(countDown);
         }
     }
-    
- 
-    
-    
+
+    function qdisplay() {
+        $("#game").addClass("gameBg");
+
+        var questionOutput = [];
+        questions.forEach(function (currentQuestion, questionNumber) {
+            var answers = [];
+            for (letter in currentQuestion.answers) {
+                answers.push(
+                    `<label class="form-check-label">
+                    <input type="radio" class="form-check-input" name="question${questionNumber}" value="${letter}">
+                    ${letter} :
+                    ${currentQuestion.answers[letter]}
+                  </label><br>`
+                );
+            }
+            // add this question and its answers to the output
+            questionOutput.push(
+                `<div class="question"> ${currentQuestion.question} </div>
+                <div class="answers"> ${answers.join('')} </div>`
+            );
+            //   console.log(questionOutput);
+        })
+        $("#quiz").html(questionOutput);
+        $("#submit").removeClass("hide");
+    }
+
+    function showResults() {
+        clearInterval(countDown);
+        // gather answer containers from our quiz
+        var answerContainers = quiz.querySelectorAll('.answers');
+
+        // for each question...
+        questions.forEach(function (currentQuestion, questionNumber) {
+
+            // find selected answer
+            var answerContainer = answerContainers[questionNumber];
+            var selector = 'input[name=question' + questionNumber + ']:checked';
+            var userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+            // if answer is correct
+            if (userAnswer === currentQuestion.correctAnswer) {
+                // add to the number of correct answers
+                correct++;
+
+            }
+            // if answer is wrong or blank
+            else {
+                incorrect++;
+            }
+        });
+
+        // show number of correct answers out of total
+        $("#quiz").html("You got " + correct + " out of " + questions.length + " correct.");
+    }
+    $("#submit").on("click", showResults);
+
 })
